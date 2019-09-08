53438a3b1fd165071b3d174ae3f5459e086126bc
kalanaelapatha <38855998+kalanaelapatha@users.noreply.github.com>
2019-09-08 17:31:46 +0530

    Chat and flight_contents

--- app/src/main/java/com/example/welcome/Flight.java
+++ app/src/main/java/com/example/welcome/Flight.java
@@ -1,14 +1,31 @@
 package com.example.welcome;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Flight extends AppCompatActivity {
+
+    private CardView pasikuda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flight);
+
+
+
+        pasikuda=(CardView)findViewById(R.id.pasikuda_id);
+
+        pasikuda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pasikuda= new Intent(getApplicationContext(),pasikudaFlight.class);
+                startActivity(pasikuda);
+            }
+        });
     }
 }
